492e76e985a8d91935de18d26aab2de86a5e6f3e
abangdsm <id.reversal@gmail.com>
2022-12-28 01:07:21 +0700

    Commit pertama

---
+++ script.js
@@ -0,0 +1,84 @@
+// PERHITUNGAN ARITMATIKA DENGAN JAVASCRIPT
+
+// let a = 12;
+// let b = 14;
+
+// let benar = a < b;
+// let salah = a > b;
+
+// console.log(salah);
+
+
+// MEMANGGIL VARIABEL
+
+// const nama = "Dwi Star Muda";
+// console.log('Nama saya adalah ' + nama);
+
+
+// TIPE DATA
+
+// let a = 'Ini adalah string';
+// let x = 5.25;
+
+// const cek = x > 10;
+
+// console.log(typeof(cek));
+
+
+// OPERATOR
+
+// let a = 2;
+// let b = 10;
+
+// a += b;
+
+// console.log(a);
+
+
+// PERCABANGAN IF/ELSE STATEMENT
+
+// const hujan = true;
+
+// if(hujan){
+//     console.log('Bawa Payung ya, nanti kamu sakit :)');
+// }else{
+//     console.log('Langsung aja berangkat. Jangan lupa berdoa ya :)');
+// }
+
+
+// SWITCH STATEMENT
+
+// let bahasa = 'indonesia';
+// let salam = '';
+
+// switch (bahasa) {
+//     case 'english' :
+//         salam = 'excuse me';
+//         break;
+//     case 'java' :
+//         salam = 'Kulonuwon';
+//         break;
+//     case 'belanda' :
+//         salam = 'halo juga!';
+//         break;
+//     default :
+//     salam = 'Selamat Pagi!';
+// }
+
+// console.log(salam);
+
+
+// PERULANGAN (LOOP)
+
+// 'for loop'
+// for(let i = 0; i <= 5; i++){
+//     console.log(i);
+// }
+
+// 'for of loop'
+// let buahSaya = ['Pisang', 'Salak', 'Semangka', 'Mangga'];
+
+// for(const arrayItem of buahSaya){
+//     console.log(arrayItem);
+// }
+
